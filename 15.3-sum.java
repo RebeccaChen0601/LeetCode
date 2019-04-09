7e5cfb19f7437502a55e0d249d62c64985f910c6
RebeccaChen0601 <mumu19980601@gmail.com>
2019-04-08 22:28:56 -0400

    first draft q15

---
+++ 15.3-sum.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=15 lang=java
+ *
+ * [15] 3Sum
+ *
+ * https://leetcode.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (23.67%)
+ * Total Accepted:    514.8K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * Given an array nums of n integers, are there elements a, b, c in nums such
+ * that a + b + c = 0? Find all unique triplets in the array which gives the
+ * sum of zero.
+ * 
+ * Note:
+ * 
+ * The solution set must not contain duplicate triplets.
+ * 
+ * Example:
+ * 
+ * 
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * 
+ * A solution set is:
+ * [
+ * ⁠ [-1, 0, 1],
+ * ⁠ [-1, -1, 2]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length == 0 || (nums.length < 3)) {
+            return list;
+        }
+        Arrays.sort(nums);
+        int i = 0, k = 1, j = nums.length - 1, ans = 0;
+        while(i + 1 != k || k + 1 != j){
+            System.out.println("i: " + i + " k: " + k + " j: " + j);
+            ans = nums[i] + nums[k] + nums [j];
+            if(ans > 0){
+                if(k + 1 < j){
+                    j--;
+                    continue;
+                }
+            } else if (ans < 0) {
+                if(k + 1 < j) {
+                    k++;
+                    continue;
+                }
+                if(i + 1 < k){
+                    i++;
+                    k = i+1;
+                    continue;
+                } 
+            } else {
+                List<Integer> intList = new ArrayList<>();
+                intList.add(nums[i]);
+                intList.add(nums[k]);
+                intList.add(nums[j]);
+                list.add(intList);
+                if(k + 1 < j) {
+                    k++;
+                    continue;
+                } else if(i + 1 < k) {
+                    i++;
+                    k = i+1;
+                    continue;
+                }
+            }      
+        }
+        return list;
+    }
+    
+    public List<List<Integer>> findThreeSum(int i, int k, int j, int[] nums, 
+                                            List<List<Integer>> list, boolean loopEnd){
+        System.out.println("i: " + i + " k: " + k + " j: " + j);
+        int ans = nums[i] + nums[k] + nums [j];
+        if(i + 1 == k && k + 1 == j) loopEnd = false;
+        if(ans > 0) {
+            if(k + 1 < j) {
+                findThreeSum(i, i+1, --j, nums, list, loopEnd);
+            }
+        } else if(ans < 0){
+            if(k + 1 < j) {
+                findThreeSum(i, ++k, j, nums, list, loopEnd);  
+            } 
+            System.out.println("2");
+            if(i + 1 < k && loopEnd) {
+                findThreeSum(++i, i+1, j, nums, list, loopEnd);
+            }
+        } else {
+            List<Integer> intList = new ArrayList<>();
+            intList.add(nums[i]);
+            intList.add(nums[k]);
+            intList.add(nums[j]);
+            list.add(intList);
+            if(k + 1 < j) {
+                findThreeSum(i, ++k, j, nums, list, loopEnd); 
+            }
+        }      
+        return list;
+    }
+}
