94fc2c462b24498636b0161f0e7be15e4c4d1daa
RebeccaChen0601 <mumu19980601@gmail.com>
2019-04-11 23:44:54 -0400

    first draft q10

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -85,17 +85,17 @@ class Solution {
     public boolean isMatch(String s, String p) {
         boolean [][] dp = new boolean[s.length()+1][p.length()+1];
         dp[s.length()][p.length()] = true;
-        for(int i = s.length(); i>= 0; i++) {
+        for(int i = s.length(); i>= 0; i--) {
             for(int j = p.length() - 1; j >= 0; j--) {
-                boolean match = s.charAt(i) == p.charAt(j) || p.charAt(j) == '.';
-                if(p.charAt(j+1)=='*'){
-                    dp[i][j] = dp[i][j+2] || match && dp[i+1][j];
+                boolean first_match = s.charAt(i) == p.charAt(j) || p.charAt(j) == '.';
+                if(j + 1 < p.length() && p.charAt(j+1)=='*'){
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
                 } else {
-                    dp[i][j] = match;
+                    dp[i][j] = first_match && dp[i+1][j+1];
                 }
             }
         }
-        return dp[s.length()][p.length()];
+        return dp[0][0];
     }
 }
 
