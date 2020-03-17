d2c4da6991086f2413b680def865b2910152ec4d
RebeccaChen0601 <mumu19980601@gmail.com>
2020-03-18 00:19:48 +0800

    update p207

--- 210.course-schedule-ii.py
+++ 210.course-schedule-ii.py
@@ -1,14 +1,12 @@
 #
-# @lc app=leetcode id=207 lang=python3
+# @lc app=leetcode id=210 lang=python3
 #
-# [207] Course Schedule
+# [210] Course Schedule II
 #
 
 # @lc code=start
 class Solution:
-    
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         if len(prerequisites) == 0:
             return True
         
@@ -21,8 +19,6 @@ class Solution:
                 mapping[first] = set()
             if second not in in_degree:
                 in_degree[second] = 0
-            if second in mapping[first]:
-                continue
             all_elem.add(first)
             all_elem.add(second)
             in_degree[second] += 1
@@ -43,8 +39,6 @@ class Solution:
                 if in_degree[neighbor] == 0:
                     queue.append(neighbor)
         
-        if(len(all_elem) != len(order)):
-            return False
         
         return True
 # @lc code=end
