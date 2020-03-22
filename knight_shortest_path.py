b40dddba043bf7911d68ef8ab6cbaa6c8dd122c0
RebeccaChen0601 <mumu19980601@gmail.com>
2020-03-22 22:06:32 +0800

    final knight version

--- knight_shortest_path.py
+++ knight_shortest_path.py
@@ -16,36 +16,44 @@ class Solution:
     def shortestPath(self, grid, source, destination):
         # write your code here
 
-        queue = collections.deque(source)
-        actions = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
+        visited = set() 
+
+        if not (self.isValid(source.x, source.y, visited, grid) or 
+        self.isValid(destination.x, destination.y, visited, grid)):
+            return -1
+
+        queue = collections.deque([(source.x, source.y)])
+        visited.add((source.x, source.y))
         pathLen = 0
-        visited = set()
+        
 
         while queue:
             pathLen += 1
             for _ in range(len(queue)):
-                position = queue.popleft()
-                for inc_x, inc_y in actions:
-                    new_x = position.x + inc_x
-                    new_y = position.y + inc_y
-                    if(new_x, new_y) == destination:
-                        return pathLen
-                    if self.isValid(new_x, new_y, position.x, position.y, visited, grid):
+                x, y = queue.popleft()
+                for inc_x, inc_y in [(1, 2), (1, -2), (-1, 2), 
+                (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]:
+                    new_x = x + inc_x
+                    new_y = y + inc_y
+                    if self.isValid(new_x, new_y, visited, grid):
+                        if new_x == destination.x and new_y == destination.y:
+                            return pathLen
                         queue.append((new_x, new_y))
                         visited.add((new_x, new_y))
         
         return -1
 
-    def isValid(self, x, y, old_x, old_y, visited, grid):
+    def isValid(self, x, y, visited, grid):
         if (x, y) in visited:
             return False
-        if not (0 <= x < len(grid) and 0 <= y < len(grid)):
+        if not (0 <= x < len(grid) and 0 <= y < len(grid[0])):
             return False
-        barrier_x = 1 if x - old_x > 0 else -1
-        barrier_y = 1 if y - old_y > 0 else -1
-        if grid[old_x + barrier_x][y] and grid[old_x][old_y + barrier_y]:
-            return False
-        return grid[x][y]
-
-
-
+        ####?????/ why don't check 蹩脚马？？？？？？？？？？？？？？？？？
+        # barrier_x = 1 if x - old_x > 0 else -1
+        # barrier_y = 1 if y - old_y > 0 else -1
+        # check_x = old_x + barrier_x
+        # check_y = old_y + barrier_y
+        # if (0 <= check_x < len(grid) and 0 <= check_y <= len(grid[0])):
+        #     if grid[old_x + barrier_x][y] and grid[old_x][old_y + barrier_y]:
+        #         return False
+        return not grid[x][y]
