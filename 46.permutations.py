fe0e0d59d02e1248e52cd489aad19e0dbe862bee
RebeccaChen0601 <mumu19980601@gmail.com>
2020-04-14 23:37:11 +0800

    q46 solved

---
+++ 46.permutations.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+
+# @lc code=start
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.result = []
+        self.dfs(nums, 0, len(nums))
+        return self.result
+
+    def dfs(self, nums, start, end):
+        if start == end:
+            self.result.append(nums[:])
+        for i in range(start, end):
+            nums[i], nums[start] = nums[start], nums[i]
+            self.dfs(nums, start + 1, end)
+            nums[i], nums[start] = nums[start], nums[i]
+        
+# @lc code=end
+
