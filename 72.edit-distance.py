5c044a5f2553eca384d2eda2d2949303b79d09be
Rebecca <mumu19980601@gmail.com>
2020-12-23 23:18:58 -0500

    update -72

--- 72.edit-distance.py
+++ 72.edit-distance.py
@@ -17,7 +17,7 @@ class Solution:
                 if word2[i-1] == word1[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
-                    dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1) 
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1 
         return dp[len(word2)][len(word1)]
         
 # @lc code=end
