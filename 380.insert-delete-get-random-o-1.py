9cfde7c8df61195a8d6a7ec7c98bf6ff936afcab
Rebecca <mumu19980601@gmail.com>
2021-01-17 23:18:34 -0500

    added 380

---
+++ 380.insert-delete-get-random-o-1.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=380 lang=python3
+#
+# [380] Insert Delete GetRandom O(1)
+#
+
+# @lc code=start
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict.keys():
+            return False  
+        self.list.append(val)
+        self.dict[val] = len(self.list) - 1
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+
+        if val in self.dict.keys():
+
+            # move the last element to the place idx of the element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            del self.dict[val]
+            self.list.pop()
+            return True
+        
+        return False
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.list)
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+# @lc code=end
+
