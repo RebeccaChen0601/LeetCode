f60120387f539e4697f5a342ae56b5116efe1558
Rebecca <mumu19980601@gmail.com>
2021-02-25 10:15:03 -0500

    update q78

--- 78.subsets.py
+++ 78.subsets.py
@@ -9,12 +9,8 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         def backtrack(subset, index):
-            if index == len(nums) + 1:
-                return
             res.append(list(subset))
             for i in range(index, len(nums)):
-                if i > 0 and nums[i] == nums[i-1]:
-                    continue
                 subset.append(nums[i])
                 backtrack(subset, i + 1)
                 subset.pop()
